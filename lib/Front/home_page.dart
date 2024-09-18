4c462c276464e3d2a07c61606673aac7b557cfbb
kevinzekee <kzekeramos@gmail.com>
2024-09-18 20:38:08 +0800

    all files

--- lib/Front/home_page.dart
+++ lib/Front/home_page.dart
@@ -68,6 +68,7 @@ class HomePage extends StatelessWidget {
           bottom: TabBar(
             tabs: [
               Tab(text: 'Notes', icon: const Icon(Icons.note, color: Color(0xFFFFDEA7))),
+              Tab(text: 'Folders', icon: const Icon(Icons.folder, color: Color(0xFFFFDEA7))),
             ],
             labelColor: const Color(0xFFFFDEA7),
             unselectedLabelColor: Colors.grey,
